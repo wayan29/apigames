21ee9a4cc7d7b557d671fb23fa7f6a44b347a049
Wayan danayasa <44164208+wayan29@users.noreply.github.com>
2023-07-18 14:30:50 +0800

    Update infoakun.php

--- infoakun.php
+++ infoakun.php
@@ -13,10 +13,10 @@ function sendGetRequest($url)
 }
 
 // URL untuk mendapatkan informasi akun
-$url = 'https://v1.apigames.id/merchant/M220512WYZE9646NK';
+$url = 'https://v1.apigames.id/merchant/XXXXXXXX';
 
 // Signature untuk permintaan
-$signature = 'd1b32ba8439e8ae4dc73abf49190e5be';
+$signature = 'XXXXXXXXXXXXXXX';
 
 // Membuat URL lengkap dengan signature
 $urlWithParams = $url . '?signature=' . urlencode($signature);
